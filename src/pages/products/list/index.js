271491c635f09c37c7ae895be77f154288c32087
Dmitry Krylov <dkrylov@bestplace.pro>
2020-10-25 15:50:03 +0300

    Products list page implementation

--- src/pages/products/list/index.js
+++ src/pages/products/list/index.js
@@ -1,36 +1,131 @@
-import ProductForm from "../../../components/product-form";
+import SortableTable from '../../../components/sortable-table/index.js';
+import DoubleSlider from '../../../components/double-slider/index.js';
+
+import header from './products-header.js';
 
 export default class Page {
   element;
-  subElements = {};
+  subElements = {
+    filterName: null,
+    filterStatus: null,
+  };
   components = {};
+  form;
+
+  initComponents() {
+    const minPrice = 0;
+    const maxPrice = 4000;
+
+    const sortableTable = new SortableTable(header, {
+      url: this.getTableUrl(minPrice, maxPrice),
+      start: 0,
+      step: 30,
+      rowHrefFunc: item => `/products/${item.id}`,
+    });
+
+    const slider = new DoubleSlider({
+      min: minPrice,
+      max: maxPrice,
+    });
+
+    this.components.sortableTable = sortableTable;
+    this.components.slider = slider;
+  }
+
+  async updateComponents(priceMin, priceMax, filterName, status) {
+    const { sorted } = this.components.sortableTable;
+    this.components.sortableTable.url = new URL(
+      this.getTableUrl(priceMin, priceMax, filterName, status), process.env.BACKEND_URL);
+    await this.components.sortableTable.sortOnServer(sorted.id, sorted.order);
+  }
+
+  getTableUrl(priceMin, priceMax, filterName, status) {
+    return `api/rest/products?_embed=subcategory.category&price_gte=${priceMin}&price_lte=${priceMax}`
+      + (filterName ? `&title_like=${encodeURIComponent(filterName)}` : '')
+      + (status ? `&status=${status}` : '');
+  }
+
+  getTemplate() {
+    return `
+      <div class="products-list">
+        <div class="content__top-panel">
+          <h1 class="page-title">Products</h1>
+          <a href="/products/add" class="button-primary">Add product</a>
+        </div>
+        <div class="content-box content-box_small">
+          <form class="form-inline">
+            <div class="form-group">
+              <label class="form-label">Filter:</label>
+              <input type="text" data-element="filterName" class="form-control" placeholder="Product name">
+            </div>
+            <div class="form-group" data-element="slider">
+              <label class="form-label">Price:</label>
+            </div>
+            <div class="form-group">
+              <label class="form-label">Status:</label>
+              <select class="form-control" data-element="filterStatus">
+                <option value="" selected="">Any</option>
+                <option value="1">Active</option>
+                <option value="0">Inactive</option>
+              </select>
+            </div>
+          </form>
+        </div>
+        <div class="products-list__container" data-element="sortableTable">
+        </div>
+      </div>
+    `;
+  }
 
   async render() {
-    const element = document.createElement('div');
+    const wrapper = document.createElement('div');
+    wrapper.innerHTML = this.getTemplate();
+    this.element = wrapper.firstElementChild;
 
-    element.innerHTML = `
-      <div>
-        <h1>List page</h1>
-      </div>`;
-
-    this.element = element.firstElementChild;
+    this.subElements = this.getSubElements(this.element);
+    this.form = this.element.querySelector('.form-inline')
 
     this.initComponents();
-    await this.renderComponents();
+    this.renderComponents();
+
+    this.initEventListeners();
 
     return this.element;
   }
 
-  initComponents() {
-    const productId = '101-planset-lenovo-yt3-x90l-64-gb-3g-lte-cernyj';
+  renderComponents() {
+    Object.keys(this.components).forEach(component => {
+      const root = this.subElements[component];
+      const { element } = this.components[component];
 
-    this.components.productFrom = new ProductForm(productId);
+      root.append(element);
+    });
   }
 
-  async renderComponents() {
-    const element = await this.components.productFrom.render();
+  getSubElements(element) {
+    const elements = element.querySelectorAll('[data-element]');
 
-    this.element.append(element);
+    return [...elements].reduce((accum, subElement) => {
+      accum[subElement.dataset.element] = subElement;
+
+      return accum;
+    }, {});
+  }
+
+  initEventListeners() {
+    this.components.slider.element.addEventListener('range-select', this.onFormChange);
+    this.subElements.filterName.addEventListener('input', this.onFormChange);
+    this.subElements.filterStatus.addEventListener('change', this.onFormChange);
+
+    this.form.addEventListener('submit', e => e.preventDefault())
+  }
+
+  onFormChange = async () => {
+    const {from, to} = this.components.slider.selected;
+    const filterName = this.subElements.filterName.value;
+    const filterStatus = this.subElements.filterStatus.value;
+
+    this.updateComponents(from, to, filterName, filterStatus);
   }
 
   destroy() {
@@ -39,3 +134,4 @@ export default class Page {
     }
   }
 }
+
