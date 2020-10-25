4cc2e6eebf6700d9db13fe88fb7aa315eb9ab5af
Dmitry Krylov <dkrylov@bestplace.pro>
2020-10-25 16:57:59 +0300

    Product edit page implementation

--- src/pages/products/edit/index.js
+++ src/pages/products/edit/index.js
@@ -1,18 +1,41 @@
+import ProductForm from '../../../components/product-form/index.js';
+
 export default class Page {
   element;
-  subElements = {};
-  components = {};
+  productId = null;
+  form;
+
+  constructor(productId) {
+    this.productId = productId;
+
+    this.form = new ProductForm(productId);
+  }
+
+  getTemplate() {
+    return `
+      <div class="products-edit">
+          <div class="content__top-panel">
+          <h1 class="page-title">
+              <a href="/products" class="link">Products</a> / ${this.productId ? 'Edit' : 'Add'}
+          </h1>
+          </div>
+          <div class="content-box"></div>
+      </div>
+    `;
+  }
 
   async render() {
-    const element = document.createElement('div');
+    const wrapper = document.createElement('div');
+    wrapper.innerHTML = this.getTemplate();
+    this.element = wrapper.firstElementChild;
 
-    element.innerHTML = `
-      <div>
-        <h1>Edit page</h1>
-      </div>`;
-
-    this.element = element.firstElementChild;
+    await this.form.render();
+    this.element.querySelector('.content-box').append(this.form.element);
 
     return this.element;
+  }
+
+  destroy() {
+    this.form.destroy();
   }
 }
